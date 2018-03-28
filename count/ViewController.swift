697964d9c800b250adf9bf2ab9f9e6190eb9ea15
Seo Sang won <33479927+zhaoSeo@users.noreply.github.com>
2018-03-29 00:35:25 +0900

    1

--- count/ViewController.swift
+++ count/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var count: Int = 0
+    
+    @IBOutlet weak var lblNumber: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,15 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func btnClick(_ sender: UIButton) {
+        count = count + 1
+        lblNumber.text = String(count)
+    }
+    @IBAction func btnResetNumber(_ sender: UIButton) {
+        count = 0
+        lblNumber.text = String(count)
+    }
+    
 
 }
 
